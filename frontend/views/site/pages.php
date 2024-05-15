67acd53db87b1e2b8554670ea2a020b05c0f6cc3
Xurshid Ergashev Ergash o'g'li <Xurshid.Ergashev@imv.uz>
2024-05-15 11:34:04 +0500

    good

--- frontend/views/site/pages.php
+++ frontend/views/site/pages.php
@@ -13,9 +13,9 @@ use yii\helpers\Html;
 
     <div class="container">
         <div class="pt-120 pb-60">
-            <h2 class="fw-bold">
+            <h1 class="fw-bold">
                 <?= $page ? $page->title : null?>
-            </h2>
+            </h1>
         </div>
         <div class="pb-100">
             <?= $page ? $page->content : null?>
