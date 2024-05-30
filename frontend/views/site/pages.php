6a1a7db3f12fbf546d214cb56f5a2d3990d5b997
Xurshid Ergashev Ergash o'g'li <Xurshid.Ergashev@imv.uz>
2024-05-30 15:49:58 +0500

    dd

--- frontend/views/site/pages.php
+++ frontend/views/site/pages.php
@@ -25,27 +25,32 @@ use yii\helpers\Html;
                 <?php if ($page->is_list != 1): ?>
                     <?= $page->content ?>
                 <?php else: ?>
-                    <?php foreach ($page->posts as $key => $post): ?>
+                    <?php foreach ($posts as $key => $post): ?>
                         <div class="col-lg-4 col-md-6 col-sm-6  wow fadeInLeft animated" data-wow-duration="1500ms"
                              data-wow-delay="<?= $key + 300 ?>ms">
                             <div class="blog-card">
-                                <div class="blog-img">
-                                    <img src="<?= $post->image != null ? '/files/' . $post->image : '/template/images/blog/b-1.png' ?>"
-                                         alt="" class="img-fluid">
+                                <div class="blog-img" style="max-height: 200px">
+                                    <?php if ($post->image != null): ?>
+                                        <img src="/files/<?= $post->image ?>" alt="" class="img-fluid">
+                                    <?php else: ?>
+                                        <div style="padding: 20px 0"></div>
+                                    <?php endif; ?>
                                     <div class="blog-date"><i
                                                 class="flaticon-calendar"></i> <?= date("d M, Y", $post->created_at) ?>
                                     </div>
                                 </div>
                                 <div class="blog-details">
-                                    <?= \yii\helpers\Html::a($post->title, ['post-details', 'id' => $post->id], ['class' => 'blog-title']) ?>
-
-                                    <div class="blog-btn">
+                                    <div><?= \yii\helpers\Html::a($post->title, ['post-details', 'id' => $post->id], ['class' => 'blog-title']) ?></div>
+                                    <div>
+                                        <?= truncate($post->content, 100) ?>
+                                    </div>
+                                    <div class="blog-btn py-3">
                                         <?= \yii\helpers\Html::a("Batafsil ko'rish", ['post-details', 'id' => $post->id], ['class' => 'btn-common-sm']) ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    <?php endforeach; ?>
+                    <?php endforeach ?>
                 <?php endif; ?>
             <?php endif; ?>
         </div>
